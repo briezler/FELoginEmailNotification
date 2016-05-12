352b8e72791d994a1d026e945a2fb879ed23876f
Cameron Llewellyn <cameron.b.llewellyn@gmail.com>
2016-05-12 13:23:54 -0500

    fix a few bugs

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,4 +3,4 @@
 if (!defined ('TYPO3_MODE'))
     die ('Access denied.');
 //hook FE login
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['felogin']['login_confirmed']['login_notification'] = 'CNETInc\feloginemailnotification\Hooks\LoginNotification->sendNotification';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['felogin']['login_confirmed']['send_notification'] = 'CNETInc\feloginemailnotification\Hooks\LoginNotification->sendNotification';

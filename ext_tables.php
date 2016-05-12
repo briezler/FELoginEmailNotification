5a0e14ddcd54e49099296f7d1bbf06ed69f56ef4
Cameron Llewellyn <cameron.b.llewellyn@gmail.com>
2016-05-12 13:32:18 -0500

    build 0.0

--- ext_tables.php
+++ ext_tables.php
@@ -2,6 +2,6 @@
 ​
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'CNETInc' . $_EXTKEY,  #vendorname (choosen by u) and ExtensionKey variable
-    'feloginemailnotification', #extension name (directory name of your ext)
+    'FELoginEmainNotification', #extension name (directory name of your ext)
     'feLoginNotification'  #speaking title of the plugin that will be displayed in the drop down menu in the backend
 );
